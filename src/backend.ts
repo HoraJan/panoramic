97a2dd3774c3fbfa10dbbd6f2b3cf6b9f6219d93
Jan Hora <jan.hora@sinnerschrader.com>
2019-11-18 13:17:42 +0100

    basic-backend-with-graphql

---
+++ src/backend.ts
@@ -0,0 +1,54 @@
+import * as express from 'express';
+import * as graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+import * as storage from 'node-persist';
+import { parseSynsetToObject, FinalObject } from './synsetParser';
+import { STORAGE_FOLDER, STORAGE_KEY } from './constants.json';
+
+const getData = async (stringified: boolean = false): Promise<FinalObject | string | void> => {
+  return storage
+    .init({ dir: STORAGE_FOLDER })
+    .then(() => storage.getItem(STORAGE_KEY))
+    .then(result => {
+      const object = parseSynsetToObject(result);
+      if (stringified) {
+        return JSON.stringify(object);
+      }
+
+      return object;
+    })
+    .catch(err => console.error(err));
+};
+
+const schema = buildSchema(`
+type Query {
+    stringifiedData: String
+    treeData: FinalObject
+  }
+  
+  type FinalObject {
+    name: String
+    size: Int
+    children: [FinalObject]
+  }  
+`);
+
+const root = {
+  stringifiedData: async () => await getData(true),
+  treeData: async () => await getData()
+};
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP(async () => ({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  }))
+);
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
